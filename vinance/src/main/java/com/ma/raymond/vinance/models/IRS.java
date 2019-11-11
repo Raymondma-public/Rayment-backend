1158119aa9dadfe683226a3c70bcef13187ad9d3
Raymondma-public <raymondma.public@gmail.com>
2019-11-11 22:12:22 +0800

    [Feature] IRF settlment

--- vinance/src/main/java/com/ma/raymond/vinance/models/IRS.java
+++ vinance/src/main/java/com/ma/raymond/vinance/models/IRS.java
@@ -1,4 +1,11 @@
 package com.ma.raymond.vinance.models;
 
+import com.ma.raymond.vinance.utils.MathUtil;
+
+import java.time.LocalDate;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
 public class IRS {
+
 }
